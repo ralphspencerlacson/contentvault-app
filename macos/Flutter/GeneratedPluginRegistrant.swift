0f0fcdbb1e9e51ded0067239c5a714ba649e485d
ralphspencerlacson <ralph.lacson@camayacoast.com>
2026-07-08 12:53:31 +0800

    feat: Setup dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import shared_preferences_foundation
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
---
+++ md/TODO.md
@@ -0,0 +1,8 @@
+Video +,- 5 secs,
+Reply comments
+
+User profile registration
+User profile upload profile picture
+Upload profile banner bg
+
+Firebase connections
